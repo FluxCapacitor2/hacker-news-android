ce61d869a8894ddd75d553009b838f7f6e7e1934
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2025-04-13 20:56:36 -0400

    Add header section on comments page

--- app/src/main/java/dev/bswanson/hackernews/composable/CommentsList.kt
+++ app/src/main/java/dev/bswanson/hackernews/composable/CommentsList.kt
@@ -3,16 +3,22 @@ package dev.bswanson.hackernews.composable
 import android.text.format.DateUtils
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -45,6 +51,7 @@ import dev.bswanson.hackernews.model.Submission
 fun CommentsList(navController: NavController, storyId: ID) {
 
     val viewModel: HNViewModel = viewModel()
+    val ctx = LocalContext.current
 
     var loaded by remember { mutableStateOf(false) }
     var submission by remember { mutableStateOf<Submission?>(null) }
@@ -65,10 +72,49 @@ fun CommentsList(navController: NavController, storyId: ID) {
 
     LazyColumn {
         item {
-            Text(submission!!.text)
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        val uri = submission?.url?.toUri()
+                        if (uri == null) return@clickable
+                        CustomTabsIntent.Builder().build().launchUrl(ctx, uri)
+                    }
+            ) {
+                Text(submission!!.title, style = MaterialTheme.typography.titleLarge)
+                if (submission?.url != null) {
+                    val uri = submission!!.url!!.toUri()
+                    if (uri.host != null) {
+                        Text(uri.host ?: "", style = MaterialTheme.typography.bodyMedium)
+                    }
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(4.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(Icons.Default.Person, "Author")
+                    Text(submission?.by ?: "")
+
+                    val timeText = DateUtils.getRelativeTimeSpanString(
+                        submission!!.time * 1000L,
+                        System.currentTimeMillis(),
+                        0L
+                    )
+
+                    Icon(Icons.Default.DateRange, "Submitted")
+                    Text(timeText.toString())
+
+                    Icon(Icons.Default.ThumbUp, "Points")
+                    Text(submission?.score?.toString().orEmpty())
+                }
+
+                Text(submission!!.text)
+            }
         }
         for (commentId in submission?.kids ?: listOf()) {
             item {
+                HorizontalDivider()
                 Comment(commentId)
             }
         }
@@ -104,11 +150,7 @@ fun Comment(commentId: ID) {
         return
     }
 
-    Column(
-        modifier = Modifier
-
-            .padding(vertical = 12.dp)
-    ) {
+    Column(modifier = Modifier.padding(top = 12.dp)) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
@@ -151,19 +193,26 @@ fun Comment(commentId: ID) {
                     }
                 }))
             } else {
-                Text("(expand)", style = MaterialTheme.typography.labelMedium)
+                Text(
+                    "(expand)",
+                    style = MaterialTheme.typography.labelMedium,
+                    modifier = Modifier.padding(vertical = 6.dp)
+                )
             }
         }
 
         if (!collapsed) {
-            Column(modifier = Modifier.drawBehind {
-                drawLine(
-                    color = Color.LightGray,
-                    start = Offset(x = 0f, y = 0f),
-                    end = Offset(x = 0f, y = size.height),
-                    strokeWidth = 1f
-                )
-            }.padding(start = 16.dp)) {
+            Column(
+                modifier = Modifier
+                    .drawBehind {
+                        drawLine(
+                            color = Color.LightGray,
+                            start = Offset(x = 0f, y = 0f),
+                            end = Offset(x = 0f, y = size.height),
+                            strokeWidth = 1f
+                        )
+                    }
+                    .padding(start = 16.dp)) {
                 for (commentId in submission?.kids ?: listOf()) {
                     Comment(commentId)
                 }
